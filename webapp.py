edc0bc92cf893ed18262d49ea2d23048d0a2c63a
zack-horton <zhorton007@gmail.com>
2024-03-04 11:36:20 -0500

    models

--- webapp.py
+++ webapp.py
@@ -20,10 +20,50 @@ train_df = pd.read_csv("training_data.csv")
 train_query = train_df['query'].values
 train_slotfilling = train_df['slot filling'].values
 
-transformer_model = keras.models.load_model("sql_transformer.keras", custom_objects={
+transformer_model1 = keras.models.load_model("sql_transformer.keras", custom_objects={
     "TransformerEncoder": TransformerEncoder,
     "PositionalEmbedding": PositionalEmbedding,
 })
+transformer_model2 = keras.models.load_model("sql_transformer_stacks2_embed256_heads16.keras", custom_objects={
+    "TransformerEncoder": TransformerEncoder,
+    "PositionalEmbedding": PositionalEmbedding,
+})
+transformer_model3 = keras.models.load_model("sql_transformer_stacks1_embed512_heads16.keras", custom_objects={
+    "TransformerEncoder": TransformerEncoder,
+    "PositionalEmbedding": PositionalEmbedding,
+})
+transformer_model4 = keras.models.load_model("sql_transformer_stacks1_embed256_heads16.keras", custom_objects={
+    "TransformerEncoder": TransformerEncoder,
+    "PositionalEmbedding": PositionalEmbedding,
+})
+transformer_model5 = keras.models.load_model("sql_transformer_stacks1_embed512_heads16_denseunits64.keras", custom_objects={
+    "TransformerEncoder": TransformerEncoder,
+    "PositionalEmbedding": PositionalEmbedding,
+})
+transformer_model6 = keras.models.load_model("sql_transformer_stacks1_embed512_heads16_denseunits32.keras", custom_objects={
+    "TransformerEncoder": TransformerEncoder,
+    "PositionalEmbedding": PositionalEmbedding,
+})
+transformer_model7 = keras.models.load_model("sql_transformer_stacks1_embed512_heads16_denseunits16.keras", custom_objects={
+    "TransformerEncoder": TransformerEncoder,
+    "PositionalEmbedding": PositionalEmbedding,
+})
+transformer_model8 = keras.models.load_model("sql_transformer_stacks1_embed512_heads16_denseunits16.keras", custom_objects={
+    "TransformerEncoder": TransformerEncoder,
+    "PositionalEmbedding": PositionalEmbedding,
+})
+
+transformers = [transformer_model1, transformer_model2, transformer_model3, 
+                transformer_model4, transformer_model5, transformer_model6, 
+                transformer_model7, transformer_model8]
+transformer_dict = {transformer_model1: "sql_transformer.keras",
+                    transformer_model2: "sql_transformer_stacks2_embed256_heads16.keras",
+                    transformer_model3: "sql_transformer_stacks1_embed512_heads16.keras",
+                    transformer_model4: "sql_transformer_stacks1_embed256_heads16.keras",
+                    transformer_model5: "sql_transformer_stacks1_embed512_heads16_denseunits64.keras",
+                    transformer_model6: "sql_transformer_stacks1_embed512_heads16_denseunits32.keras",
+                    transformer_model7: "sql_transformer_stacks1_embed512_heads16_denseunits16.keras",
+                    transformer_model8: "sql_transformer_stacks1_embed512_heads8_denseunits16.keras"}
 
 stock_data = pd.read_csv('prototype_table.csv')
 #####################################################
@@ -65,31 +105,34 @@ run_query_button = st.button("Run query",
 
 if run_query_button:
     formatted_prompt = re.sub(r'[0-9]', '', prompt)
-    slot_filling = predict_slots_query(formatted_prompt,
-                                       transformer_model,
-                                       vectorize_query_text,
-                                       vectorize_slot_text)
-    
-    return_df, written_query, all_data = SlotParser(slot_filling,
-                                                    prompt,
-                                                    stock_data)
-    return_df.columns = [" ".join([x.capitalize() for x in col.split("_")]) for col in return_df.columns]
-    st.data_editor(return_df,
-                   hide_index=True,
-                   use_container_width=True,
-                   disabled=True)
-    
-    st.write("Prompt you submitted:")
-    st.info(prompt)
-    
-    st.write("Slot filling procedure (removes number requests):")
-    st.info(slot_filling)
-    
-    st.write("SQL Query:")
-    st.info(written_query)
-    
+    for transformer_model in transformers:
+        st.success(f"Using: {transformer_dict[transformer_model]}")
+        slot_filling = predict_slots_query(formatted_prompt,
+                                        transformer_model,
+                                        vectorize_query_text,
+                                        vectorize_slot_text)
+        
+        return_df, written_query, all_data = SlotParser(slot_filling,
+                                                        prompt,
+                                                        stock_data)
+        return_df.columns = [" ".join([x.capitalize() for x in col.split("_")]) for col in return_df.columns]
+        st.data_editor(return_df,
+                    hide_index=True,
+                    key=transformer_dict[transformer_model],
+                    use_container_width=True,
+                    disabled=True)
+        
+        st.write("Prompt you submitted:")
+        st.info(prompt)
+        
+        st.write("Slot filling procedure (removes number requests):")
+        st.info(slot_filling)
+        
+        st.write("SQL Query:")
+        st.info(written_query)
+        
     st.write("Entire Table:")
     st.data_editor(all_data,
-                   hide_index=True,
-                   use_container_width=True,
-                   disabled=True)
+                hide_index=True,
+                use_container_width=True,
+                disabled=True)
